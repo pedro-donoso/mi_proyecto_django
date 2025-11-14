43833e039bc1dfdd4b0624d127f1915dc69ee1ec
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-14 11:48:32 -0300

    creo vista

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Libro
+
+def lista_libros(request):
+    libros = Libro.objects.all()
+    return render(request, 'catalogo/lista_libros.html', {'libros': libros})
